6b4febadbfe4544b3c74ffd58a70daad54152aa6
maoli <3457127257@qq.com>
2026-05-07 15:01:45 +0800

    new

--- Exchangeapp_backend/main.go
+++ Exchangeapp_backend/main.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
 	"Exchangeapp/config"
 	"Exchangeapp/router"
 )
@@ -14,5 +21,27 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
-	r.Run(port) // listen and serve on 0.0.0.0:8080
+
+	srv := &http.Server{
+		Addr:    port,
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
